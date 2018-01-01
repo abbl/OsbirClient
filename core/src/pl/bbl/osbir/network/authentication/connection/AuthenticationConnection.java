f911ec7e062b4279811863ff843a4c887d0a60d0
Patrick <bleowy1@gmail.com>
2018-01-01 03:52:46 +0100

    Started working on network side of client.

--- core/src/pl/bbl/osbir/network/authentication/connection/AuthenticationConnection.java
+++ core/src/pl/bbl/osbir/network/authentication/connection/AuthenticationConnection.java
@@ -1,9 +1,29 @@
 package pl.bbl.osbir.network.authentication.connection;
 
+import io.netty.channel.ChannelPipeline;
 import pl.bbl.network.client.AbstractClient;
+import pl.bbl.network.server.handlers.PacketHandler;
+import pl.bbl.network.server.handlers.PacketReceiver;
+import pl.bbl.osbir.network.authentication.receivers.AuthenticationReceivers;
 
 public class AuthenticationConnection extends AbstractClient{
     public AuthenticationConnection(String host, int port) {
         super(host, port);
+    }
+
+    @Override
+    protected void addHandlersToChannel(ChannelPipeline pipeline) {
+        super.addHandlersToChannel(pipeline);
+        PacketHandler packetHandler = new PacketHandler();
+        pipeline.addLast("PACKET_HANDLER", packetHandler);
+        AuthenticationReceivers.createReceivers(packetHandler, this);
+    }
+
+    public PacketReceiver getPacketReceiver(){
+        
+    }
+
+    private PacketHandler getPacketHandler(){
+
     }
 }
--- core/src/pl/bbl/osbir/network/authentication/receivers/AuthenticationReceivers.java
+++ core/src/pl/bbl/osbir/network/authentication/receivers/AuthenticationReceivers.java
@@ -1,11 +1,13 @@
 package pl.bbl.osbir.network.authentication.receivers;
 
+import pl.bbl.network.server.handlers.PacketHandler;
+import pl.bbl.osbir.network.authentication.communication.information.receiver.AuthenticationReceiver;
 import pl.bbl.osbir.network.authentication.connection.AuthenticationConnection;
 
 public class AuthenticationReceivers {
-    public static final String INFORMATION_RECEIVER = "INFORMATION_PACKETS";
+    public static final String AUTHENTICATION_RECEIVER = "AUTHENTICATION_PACKETS";
 
-    public static void createReceivers(AuthenticationConnection authenticationConnection){
-
+    public static void createReceivers(PacketHandler packetHandler, AuthenticationConnection authenticationConnection){
+        packetHandler.addReceiver(new AuthenticationReceiver(AUTHENTICATION_RECEIVER, authenticationConnection));
     }
 }
